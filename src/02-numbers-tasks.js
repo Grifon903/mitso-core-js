62ae80edcf829f500225669805cbc5ec9cf0f968
student <student@mitso.by>
2024-11-11 19:11:55 +0300

    02-4

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -68,10 +68,11 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const first = x2 - x1;
+  const second = y2 - y1;
+  return Math.sqrt(first ** 2 + second ** 2);
 }
-
 /**
  * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
  *
