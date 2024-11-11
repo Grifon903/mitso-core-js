81d9c09c4e3b41821bffe71159f372333fc342e6
student <student@mitso.by>
2024-11-11 19:11:20 +0300

    02-2

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -33,10 +33,9 @@ function getRectangleArea(width, height) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
 }
-
 /**
  * Returns an average of two given numbers.
  *
